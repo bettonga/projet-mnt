40cdd5fcec19903cb95d513f282e401b65d1ad57
Gabriel Betton <gabriel.betton@gmail.com>
2021-12-05 15:41:03 +0100

    Copy of .txt, proj to come

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <stdio.h>
+#include <proj.h>
+#include <string.h>
+
+int proj93(FILE * output);
 
 int main() {
-  std::cout << "yoyoyyo" << std::endl;
+  FILE * data_proj;
+  data_proj = fopen("../projet-mnt/datas/MNT_lilrade_PROJ.txt" , "w");
+  proj93(data_proj);
+  fclose(data_proj);
+}
+
+
+int proj93(FILE * output) {
+  FILE * data;
+  char line [100];
+
+  data = fopen("../projet-mnt/datas/MNT_lilrade.txt" , "r");
+  if (data == NULL) perror ("Error opening file");
+  else {
+    while ( fgets (line , sizeof(line) , data) != NULL ) {       // lit la ligne du fichier de départ
+      fprintf(output, line, 1);                                  // écrit les coordonnées projetées dans le fichier
+    }
+    fclose (data);
+  }
+  return 0;
 }
