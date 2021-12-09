b2b1978cd2d1ca2239fae06407db1a87c43db4d1
bettonga <gabriel.betton@ensta-bretagne.org>
2021-12-09 11:35:25 +0100

    proj successful

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include <stdio.h>
 #include <proj.h>
-#include <string.h>
 
 using namespace std;
 
@@ -10,7 +8,8 @@ int proj93(fstream& output);
 
 int main() {
   fstream data_proj;
-  data_proj.open("../projet-mnt/datas/MNT_lilrade_PROJ.txt" , fstream::out);
+  data_proj.precision(11);    // precision of raw datas
+  data_proj.open("../datas/MNT_lilrade_PROJ.txt" , fstream::out);
   proj93(data_proj);
   data_proj.close();
 }
@@ -19,19 +18,19 @@ int main() {
 
 int proj93(fstream& output) {
   fstream data;
-  char line [100];
   double lat;
   double lon;
   double z;
 
-  data.open("../projet-mnt/datas/MNT_lilrade.txt" , fstream::in);
+  data.open("../datas/MNT_lilrade.txt" , fstream::in);
 
   // initialisation de la projection
   PJ *P;
   PJ_COORD c, c_out;
+  // x_0 et y_0 première mesure, par défaut: +x_0=700000 +y_0=6600000
   P = proj_create_crs_to_crs(PJ_DEFAULT_CTX,
                              "+proj=longlat +datum=WGS84",
-                             "+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs",
+                             "+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=-6744040 +y_0=4381740 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs",
                              NULL);
   if (P==0){return 1;}
   else {
@@ -39,27 +38,22 @@ int proj93(fstream& output) {
     if( 0 == P_for_GIS ) {proj_destroy(P); return 1;}
     proj_destroy(P);
     P = P_for_GIS;
+
+    //lecture du fichier de données
+    while (data >> lat >> lon >> z){
+      // projection de chaque coordonnées
+      c.lpz.lam = lat;
+      c.lpz.phi = lon;
+      c.lpz.z = z;
+      c_out = proj_trans(P, PJ_FWD, c);
+      // écriture du fichier output
+      output << c_out.xyz.x << " " << c_out.xyz.y << " " << c_out.xyz.z << endl;
+    }
+
+
+
+    proj_destroy(P);
   }
-  for (int i=0; i<5; i++){
-    data >> lat >> lon >> z;
-    std::cout << lat << " " << lon << " " << z << std::endl;
-  }
-
-
-  // projection de chaque coordonnées
-  // while ( fgets (line , sizeof(line) , data) != NULL ) {       // lit la ligne du fichier de départ
-  //   char * lineToken = strtok(line, " ");
-  //   c.lpz.lam = atof(lineToken.c_str);
-  //   lineToken = strtok(NULL, " ");
-  //   c.lpz.phi = atof(lineToken.c_str);
-  //   lineToken = strtok(NULL, " ");
-  //   c.lpz.z = atof(lineToken.c_str);
-  //   c_out = proj_trans(P, PJ_FWD, c);
-  //   printf("%.2f\t%.2f\n", c_out.xy.x, c_out.xy.y);
-  //   fprintf(output, "% % %", c_out.xyz.x, c_out.xyz.y, c_out.xyz.z, 1);                                  // écrit les coordonnées projetées dans le fichier
-  // }
-
-  proj_destroy(P);
   data.close();
   return 0;
 }
