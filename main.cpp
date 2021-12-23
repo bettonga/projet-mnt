f7bb9c6f0d504b9d6bc293714eaf6537940f3b9f
bettonga <gabriel.betton@ensta-bretagne.org>
2021-12-23 11:45:22 +0100

    point locate and interpolation successful

--- main.cpp
+++ main.cpp
@@ -1,23 +1,50 @@
 #include <iostream>
 #include <fstream>
 #include <proj.h>
-#include
+#include <cstdio>
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Projection_traits_xy_3.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Projection_traits_xy_3<K>  Gt;
+typedef CGAL::Delaunay_triangulation_2<Gt> Delaunay;
+
+typedef Delaunay::Face_handle Face_handle;
+typedef Delaunay::Vertex_handle Vertex_handle;
+typedef K::Point_3   Point_3;
 
 using namespace std;
 
+
+
 int proj93(fstream& output);
+
+double get_z(Delaunay& dt, double x, double y, Face_handle& old_fh);
 
 int main() {
   // projection des coordonnées GPS, WGS84 -> xyz cartésien
   fstream data_proj;
+
   data_proj.precision(11);    // precision of raw datas
   data_proj.open("../datas/MNT_lilrade_PROJ.txt" , fstream::out);
   proj93(data_proj);
-
-
-
-
   data_proj.close();
+
+  ifstream in("../datas/MNT_lilrade_PROJ.txt");
+  istream_iterator<Point_3> begin(in);
+  istream_iterator<Point_3> end;
+
+  Delaunay dt(begin, end);
+  cout << dt.number_of_vertices() << endl;
+
+  Face_handle old_fh = NULL;
+  double z = get_z(dt, 15.0, 2.0, old_fh);
+  cout << z << endl;
+
+  return EXIT_SUCCESS;
+
 }
 
 
@@ -59,4 +86,18 @@ int proj93(fstream& output) {
   }
   data.close();
   return 0;
+}
+
+double get_z(Delaunay& dt, double x, double y, Face_handle& old_fh){
+  Point_3 p(x,y,0.0);
+  Face_handle fh = dt.locate(p, old_fh);
+  old_fh = fh;
+  Point_3 a = fh->vertex(0)->point();
+  Point_3 b = fh->vertex(1)->point();
+  Point_3 c = fh->vertex(2)->point();
+  double denom = (c[0]-a[0])*(b[1]-a[1]) - (c[1]-a[1])*(b[0]-a[0]);
+  double mu = (  (b[1]-a[1])*(x-a[0]) - (b[0]-a[0])*(y-a[1]) ) / denom;
+  double la = ( -(c[1]-a[1])*(x-a[0]) + (c[0]-a[0])*(y-a[1]) ) / denom;
+  double z = a[2] + la*(b[2]-a[2]) + mu*(c[2]-a[2]);
+  return z;
 }
